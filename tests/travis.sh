601bff7afa84871a9327608a1e26671f08484573
Ian Denhardt <ian@zenhack.net>
2016-12-20 11:32:06 -0500

    Largely rewrite ./test/travis.sh
    We now use the docker container to set up boulder, which subsumes most
    of the logic in the script.
    
    This also adds a (generous) timeout when waiting for boulder to start;
    while attempting to get the old script to work, I hit a couple cases
    where boulder was dying on startup, and Travis just never stopped.

--- tests/travis.sh
+++ tests/travis.sh
@@ -9,28 +9,25 @@ set -xe
 SERVER=http://localhost:4000/directory
 PORT=5002
 
-integration_install() {
-  # Helps with diagnostics to have this in the output:
-  go version
+setup_boulder() {
+  # Per the boulder README:
+  docker_ip=$(ifconfig docker0 | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}')
 
-  # `/...` avoids `no buildable Go source files` errors, for more info
-  # see `go help packages`
-  go get -d github.com/letsencrypt/boulder/... &
-  (wget https://github.com/jsha/boulder-tools/raw/master/goose.gz && \
-   mkdir $GOPATH/bin && \
-   zcat goose.gz > $GOPATH/bin/goose && \
-   chmod +x $GOPATH/bin/goose) &
-  # listenbuddy is needed for ./start.py
-  go get github.com/jsha/listenbuddy &
-
-  wait
-
+  export GOPATH=${HOME?}/go
+  git clone --depth=1 https://github.com/letsencrypt/boulder \
+    $GOPATH/src/github.com/letsencrypt/boulder
   cd $GOPATH/src/github.com/letsencrypt/boulder
-  ./test/create_db.sh
-  go run cmd/rabbitmq-setup/main.go -server amqp://localhost
-  ./start.py &
+  docker-compose pull
+  docker pull letsencrypt/boulder-tools
+  docker-compose build
+  docker-compose run \
+    -e FAKE_DNS=$docker_ip \
+    --service-ports \
+    boulder &
   cd -
+}
 
+setup_webroot() {
   mkdir public_html
   cd public_html
   if python -V 2>&1 | grep -q "Python 3."; then
@@ -39,18 +36,18 @@ integration_install() {
     python -m SimpleHTTPServer ${PORT?} &
   fi
   cd -
-
-  while ! curl ${SERVER?} >/dev/null 2>&1; do
-    printf .
-    sleep 5
-  done
-  echo
 }
 
-integration_script() {
-  . .tox/$TOXENV/bin/activate
-  pip -V
-  simp_le -v --integration_test
+wait_for_boulder() {
+  i=0
+  while ! curl ${SERVER?} >/dev/null 2>&1; do
+    if [ $(($i * 5)) -gt $((5 * 60)) ]; then
+      printf 'Boulder has not started for 5 minutes, timing out.\n'
+      exit 1
+    fi
+    i=$((i + 1))
+    sleep 5
+  done
 }
 
 if [ "py${TOXENV#py}" = "${TOXENV}" ]; then
@@ -58,22 +55,21 @@ if [ "py${TOXENV#py}" = "${TOXENV}" ]; then
 fi
 
 case $1 in
-  before_install)
-    if [ "x$BOULDER_INTEGRATION" != "x" ]; then
-      eval "$(gimme 1.5.1)"
-    fi
-  ;;
   install)
     pip install tox
     if [ "x$BOULDER_INTEGRATION" != "x" ]; then
-      integration_install
+      setup_boulder
+      setup_webroot
+      wait_for_boulder
     fi
     ;;
   script)
     export TOXENV
     tox
     if [ "x$BOULDER_INTEGRATION" != "x" ]; then
-      integration_script
+      . .tox/$TOXENV/bin/activate
+      pip -V
+      simp_le -v --integration_test
     fi
     ;;
 esac
