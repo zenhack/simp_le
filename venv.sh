fe317f6166248f40451a873fadbea76ec038dac9
Ian Denhardt <ian@zenhack.net>
2017-04-05 14:00:17 -0400

    venv.sh: Fixes for very old versions of Python.
    Debian Wheezy in particular.
    
    * Upgrade pip before setuptools; the system's pip can't handle
      current setuptools
    * Remove distribute from the venv, which causes problems

--- venv.sh
+++ venv.sh
@@ -4,8 +4,15 @@ set -e
 
 virtualenv --no-site-packages venv
 export PATH="$PWD/venv/bin:$PATH"  # #49, activate script requires bash
-for pkg in setuptools pip wheel
+for pkg in pip setuptools wheel
 do
   pip install -U "${pkg?}"
 done
+
+# Old systems that still have distribute will choke on some of the
+# newer libraries, But once we've set up setuptools we can just remove
+# it. The `|| true` is for newer systems with no distribute -- if the
+# package isn't present this command will fail.
+pip uninstall -y distribute || true
+
 pip install -e .
