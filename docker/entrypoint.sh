bda905191be35e664253744d4233f8ba94b5defd
Nicolas Duchon <nicolas.duchon@gmail.com>
2017-09-02 20:30:09 +0200

    Add custom Docker entrypoint

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# following recommendations for a consistent interface:
+# https://github.com/docker-library/official-images#consistency
+
+# if the first argument given to the entrypoint script is an
+# hyphenated flag, execute simp_le with this(those) flag(s),
+# else default to run whatever the user wanted like "bash"
+
+case "$1" in
+    -*) exec simp_le "$@" ;;
+    *) exec "$@" ;;
+esac
