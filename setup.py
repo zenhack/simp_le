9d657a7436cd0d85024f4cec7c0956587d71e5c8
Nicolas Duchon <nicolas.duchon@gmail.com>
2019-05-14 16:56:40 +0200

    Upgrade ACME to 0.34

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires = [
     # * https://github.com/pypa/pip/issues/988
     'idna<2.8',
 
-    'acme>=0.33,<0.34',
+    'acme>=0.34.2,<0.35',
     'cryptography',
     # formerly known as acme.jose:
     'josepy',
